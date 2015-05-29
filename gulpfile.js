a6903b5886889c50ea1dc097a9018042fd2aadb3
Jason Miller <developit@users.noreply.github.com>
2015-05-28 20:22:08 -0400

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rename = require('gulp-rename'),
+	babel = require('gulp-babel');
+
+gulp.task('babel', function() {
+	return gulp.src('**/*.es6')
+		.pipe( sourcemaps.init() )
+		.pipe( babel({
+			modules: 'umd',
+			loose: 'all'
+		}) )
+		.pipe( sourcemaps.write() )
+		.pipe( rename({ extname:'.js' }) )
+		.pipe( gulp.dest('./') );
+});
+
+gulp.task('watch', function() {
+	gulp.watch('**/*.es6', ['babel']);
+});
+
+gulp.task('default', ['babel']);
+gulp.task('build-watch', ['default', 'watch']);
